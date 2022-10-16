ccae80681f1438be621fb07680af8316ab9b0650
Summira Hussain <summira508411@gmail.com>
2022-10-16 18:55:28 +0100

    more java work

---
+++ StringMethods/ToUppercaseToLowercsse/Hashtag.java
@@ -0,0 +1,14 @@
+package StringMethods.ToUppercaseToLowercsse;
+
+public class Hashtag {
+  
+    public static void main(String[] args) {
+      
+      String hashtag = "#100DaysOfCode";
+      
+      // Make the hashtag lowercase:
+      System.out.println(hashtag.toLowerCase());
+      
+    }
+    
+  }
