1543fd8a0d64cba625fc115e09d408574e8a6790
Summira Hussain <summira508411@gmail.com>
2022-10-16 17:49:09 +0100

    more learning

---
+++ StringMethods/equals/Password.java
@@ -0,0 +1,16 @@
+package StringMethods.equals;
+
+public class Password {
+  
+    public static void main(String[] args) {
+      
+      String password = "correcthorsebatterystaple";
+      
+      // Write the code:
+      System.out.println(password.equals("Tr0ub4dor&3"));
+      
+    }
+    
+  }
+    
+
