c000d7129ed70ae96a799d5ce4e6a8ba19fe6cc1
Summira Hussain <summira508411@gmail.com>
2022-10-19 08:08:44 +0100

    work

---
+++ Inheritance/childpass/BiangBiang.java
@@ -0,0 +1,11 @@
+package Inheritance.childpass;
+
+public class BiangBiang extends Noodle {
+  
+    BiangBiang() {
+      
+      super(50.0, 5.0, "flat", "high-gluten flour, salt, water");
+      
+    }
+    
+  }
